4b2135e10c0f3f7d945628e6b42de77ee44bf6f6
Jaden Balogh <jadenbalogh@gmail.com>
2021-08-22 14:50:12 -0700

    Update walls

--- Assets/Scripts/Game/System/WallSpawner.cs
+++ Assets/Scripts/Game/System/WallSpawner.cs
@@ -5,14 +5,22 @@ using UnityEngine;
 public class WallSpawner : MonoBehaviour
 {
     [SerializeField] private SpriteRenderer wallPrefab;
+    [SerializeField] private SpriteRenderer wallBackgroundPrefab;
     [SerializeField] private float edgeWidth = 0.4f;
-
-    private float xOffset, yOffset;
+    [SerializeField] private float edgeDist = 2f;
 
     void Start()
     {
-        xOffset = Camera.main.orthographicSize * Camera.main.aspect - edgeWidth;
-        yOffset = Camera.main.orthographicSize - edgeWidth;
+        float xOffset = Camera.main.orthographicSize * Camera.main.aspect - edgeDist / 2f;
+        float yOffset = Camera.main.orthographicSize - edgeDist / 2f;
+
+        SpawnBGWall(yOffset * Vector2.up);
+        SpawnBGWall(yOffset * Vector2.down);
+        SpawnBGWall(xOffset * Vector2.right);
+        SpawnBGWall(xOffset * Vector2.left);
+
+        xOffset -= (edgeWidth + edgeDist) / 2f;
+        yOffset -= (edgeWidth + edgeDist) / 2f;
 
         SpawnWall(yOffset * Vector2.up);
         SpawnWall(yOffset * Vector2.down);
@@ -22,10 +30,18 @@ public class WallSpawner : MonoBehaviour
 
     private void SpawnWall(Vector2 offset)
     {
-        SpriteRenderer wall = Instantiate(wallPrefab, offset, Quaternion.identity);
-        float width = offset.y != 0 ? Camera.main.orthographicSize * Camera.main.aspect * 2 : edgeWidth;
-        float height = offset.x != 0 ? Camera.main.orthographicSize * 2 : edgeWidth;
+        SpriteRenderer wall = Instantiate(wallPrefab, offset, Quaternion.identity, transform);
+        float width = offset.y != 0 ? (Camera.main.orthographicSize * Camera.main.aspect - edgeDist) * 2 : edgeWidth;
+        float height = offset.x != 0 ? (Camera.main.orthographicSize - edgeDist) * 2 : edgeWidth;
         wall.size = new Vector2(width, height);
         wall.GetComponent<BoxCollider2D>().size = new Vector2(width, height);
+    }
+
+    private void SpawnBGWall(Vector2 offset)
+    {
+        SpriteRenderer wallBG = Instantiate(wallBackgroundPrefab, offset, Quaternion.identity, transform);
+        float bgWidth = offset.y != 0 ? (Camera.main.orthographicSize * Camera.main.aspect) * 2 : edgeDist;
+        float bgHeight = offset.x != 0 ? (Camera.main.orthographicSize) * 2 : edgeDist;
+        wallBG.size = new Vector2(bgWidth, bgHeight);
     }
 }
